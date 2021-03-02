c141237f9fabad2d468129b2d3643a125c53c484
Maria Barkouzou <mbarkouzou@gmail.com>
2021-03-02 16:27:38 +0200

    Refactor index.html and etch.js

---
+++ etch.js
@@ -0,0 +1,13 @@
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+makeRows(16, 16);
