547206dcdaffb1c7baff87836cdcfc8be15d5d72
Maria Barkouzou <mbarkouzou@gmail.com>
2021-03-02 22:41:03 +0200

    Refactor index.html and etch.js

--- etch.js
+++ etch.js
@@ -1,13 +1,47 @@
-const container = document.getElementById("container");
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
+const grid = document.querySelector(".container");
+const userInput = document.getElementById("quantity");
+const resetButton = document.querySelector(".reset");
+
+createGrid = () => {
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
+  }
 };
 
-makeRows(16, 16);
+updateGrid = () => {
+  grid.innerHTML = "";
+  grid.style.setProperty(
+    "grid-template-columns",
+    `repeat(${userInput.value}, 2fr)`
+  );
+  grid.style.setProperty(
+    "grid-template-rows",
+    `repeat(${userInput.value}, 2fr)`
+  );
+  for (let i = 0; i < userInput.value * userInput.value; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
+  }
+  console.log(userInput.value);
+};
+
+const square = document.querySelector("div");
+square.addEventListener("mouseover", function(event) {
+  event.target.classList.replace("square", "color");
+});
+
+userInput.addEventListener("change", updateGrid);
+
+resetButton.addEventListener("click", function() {
+  grid.innerHTML = "";
+  userInput.value = "";
+  grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
+  grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
+  createGrid();
+});
+
+createGrid();
